016aea388a47e907fbbe7ef111704d8c8b35f8f8
skiado <70379186+skiado@users.noreply.github.com>
2021-10-21 18:31:58 +0300

    Delete Create_Image directory

--- Create_Image/range01.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-im1 = Image.new('L',(256,256))
-print(im1.size)
-for i in range(im1.size[0]):
-	for j in range(im1.size[1]):
-		im1.putpixel((i,j),i)
-im1.show()
--- Create_Image/x01.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-im1 = Image.new('L',(300,300))
-print(im1.size)
-for i in range(im1.size[0]):
-	im1.putpixel((i,i),255)
-	im1.putpixel((i,im1.size[0]-i-1),255)
-im1.show()
